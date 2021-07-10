114a45a23fe54506e4a85e6893f4ddb015efaffa
Nwabuokei Nnamdi <nwabuokeinnamdi@gmail.com>
2021-07-10 15:30:56 +0100

    updated the attribute keys for checkoutForm function

--- assets/js/paga-checkout.js
+++ assets/js/paga-checkout.js
@@ -5,7 +5,7 @@ function checkoutForm() {
     pagaForm.setAttribute('method','POST');
     const scriptTag  = document.createElement('script');
     scriptTag.type = 'text/javascript';
-    const attributeKeys=['data-public_key','data-display_name','data-charge_url','data-display_image','data-display_tagline','data-email','data-amount','data-payment_reference','data-currency','data-phone_number','src']
+    const attributeKeys=['data-public_key','data-display_name','data-charge_url','data-redirect_url_method','data-display_image','data-display_tagline','data-email','data-amount','data-payment_reference','data-currency','data-phone_number','src']
     const attributeValues = Object.values(wc_paga_checkout_params);
     for (let index = 0; index < attributeValues.length; index++) {
         scriptTag.setAttribute(attributeKeys[index],attributeValues[index]);
